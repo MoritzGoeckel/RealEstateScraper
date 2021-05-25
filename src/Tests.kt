ab733418fb103eaf11f362ecdf32dbd6f0c94751
Moritz Goeckel <moritz.goeckel@gmail.com>
2021-05-25 19:56:29 +0200

    Create project and .gitignore

---
+++ src/Tests.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Tests {
+    fun main() {
+        println("Hello world!")
+    }
+
+    @Test
+    fun someThing(){
+        assertEquals("0", "1");
+    }
+}
