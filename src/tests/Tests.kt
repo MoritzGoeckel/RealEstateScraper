c9dfcbe70910e6fef49713a07b1c9902fd1ca755
Moritz Goeckel <moritz.goeckel@gmail.com>
2021-06-04 15:27:53 +0200

    Add gson

--- src/tests/Tests.kt
+++ src/tests/Tests.kt
@@ -5,6 +5,12 @@ import ingest.Downloader
 import ingest.portals.Ebay
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
+import com.google.gson.GsonBuilder
+
+fun prettyPrint(any: Any): String {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    return gson.toJson(any)
+}
 
 class Tests {
     @Test
@@ -12,6 +18,9 @@ class Tests {
         val downloader: Downloader = Ebay()
         val homes = downloader.download("", Contract.Buy, 6)
         val avgFaultiness = homes.sumOf { it.faultiness() } / homes.size
+
+        homes.map(::prettyPrint)
+             .map(::println)
 
         assertTrue { avgFaultiness < 0.05 }
         assertTrue { homes.size >= 20 }
